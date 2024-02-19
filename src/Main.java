4f1b7ffdc4d86c0ac6221decb86673db4019a83f
daniel12142007 <danijelahatzanov@gmail.com>
2024-02-19 14:58:49 +0600

    I finish question 1071

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(mergeAlternately("abc", "pgr"));
+
+    }
+
+    //1071. Greatest Common Divisor of Strings(00:48:39)
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+        int gbcValue = gcb(str1.length(), str2.length());
+        return str2.substring(0, gbcValue);
+    }
+
+    int gcb(int x, int y) {
+        if (y == 0)
+            return x;
+        else return gcb(y, x % y);
     }
 
     //    1768. Merge Strings Alternately(00:08:38)
-    public static String mergeAlternately(String word1, String word2) {
+    public String mergeAlternately(String word1, String word2) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < Math.max(word1.getBytes().length, word2.getBytes().length); i++) {
             if (i < word1.getBytes().length) {
